ed4104ebd51242d3fc58f1fd17b9e0743e4c85eb
Hrnciarp <itspeterh6@gmail.com>
2024-01-24 19:03:22 +0100

    pridanie obchodu

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/obchod', function () {
+    return view('obchod');
+});
+
 require __DIR__.'/auth.php';
